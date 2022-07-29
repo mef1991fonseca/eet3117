f50301b5b7f69d829f716b7ba4a41106597e0e24
mef1991fonseca <mef1991.fonseca@gmail.com>
2022-07-29 20:07:24 -0300

    primer commit

---
+++ asset/js/main.js
@@ -0,0 +1,79 @@
+let nav = document.getElementById('nav');
+let menu = document.getElementById('enlaces');
+let abrir = document.getElementById('open');
+let botones = document.getElementsByClassName('btn-header');
+let cerrado = true;
+
+/*
+function cambiar () {
+  document.getElementById('logo-cambiar').src = "logo.png";
+ }
+ 
+ function volver () {
+  document.getElementById('logo-cambiar').src = "logo.png";
+ }
+*/
+function menus(){
+    let Desplazamiento_Actual = window.pageYOffset;
+
+    if(Desplazamiento_Actual <= 150){
+        nav.classList.remove('nav2');
+        nav.className = ('nav1');
+        nav.style.transition = '1s';
+        //volver();
+        menu.style.top = '80px';
+        abrir.style.color = '#fff';
+    }else{
+        nav.classList.remove('nav1');
+        nav.className = ('nav2');
+        nav.style.transition = '1s';
+        //cambiar();
+        menu.style.top = '100px';
+        abrir.style.color = '#1c4b99';
+    }
+}
+
+function apertura(){
+    if(cerrado){
+        menu.style.width = '70vw';
+        cerrado = false;
+    }else{
+        menu.style.width = '0%';
+        menu.style.overflow = 'hidden';
+        cerrado = true;
+    }
+}
+
+window.addEventListener('load', function(){
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+    menus();
+});
+
+window.addEventListener('click',function(e){
+    if(cerrado==false){
+        let span = document.querySelector('span');
+        if(e.target !== span && e.target !== abrir){
+            menu.style.width = '0%';
+            menu.style.overflow = 'hidden';
+            cerrado = true;
+        }
+    }
+})
+
+window.addEventListener('scroll', function(){
+    console.log(window.pageYOffset);
+    menus();
+});
+
+window.addEventListener('resize', function(){
+    if(screen.width>=700){
+        cerrado = true;
+        menu.style.removeProperty('overflow');
+        menu.style.removeProperty('width');
+    }
+})
+
+abrir.addEventListener('click',function(){
+    apertura();
+});
